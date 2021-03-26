f367a262f03d9400eb7903cb56b9059b76076512
Ben <ben.dalton@madetech.com>
2021-03-26 10:37:36 +0000

    moved html back into entities_js to fix issues rendering title, description

--- pages/_includes/entities_js.11ty.js
+++ pages/_includes/entities_js.11ty.js
@@ -1,3 +1,5 @@
+module.exports = Entities;
+
 class Entities {
     data() {
         return {
@@ -7,12 +9,29 @@ class Entities {
     }
     render(data) {
         const relationships = '<ul>' + data.relationships.map(
-            relationship => 
-            `<li> ${relationship.relationship} <a href="${
-                this.url(data.collections.entities.find(entity => 
-                    entity.fileSlug === relationship.entity).url)}">${relationship.entity}</a></li>`
+            relationship =>
+                `<li> ${relationship.relationship} <a href="${
+                    this.url(data.collections.entities.find(entity =>
+                        entity.fileSlug === relationship.entity).url)}">${relationship.entity}</a></li>`
         ).join("\n") + '</ul>';
         return `
+<div class="govuk-breadcrumbs lbh-breadcrumbs lbh-container">
+    <ol class="govuk-breadcrumbs__list">
+        <li class="govuk-breadcrumbs__list-item"><a class="govuk-breadcrumbs__link" href=${this.url("/")}>Home</a></li>
+        <li class="govuk-breadcrumbs__list-item"><a class="govuk-breadcrumbs__link" href=${this.url("/dictionary")}>Data Dictionary</a></li>
+        <li class="govuk-breadcrumbs__list-item" aria-current="page">${data.title}</li>
+    </ol>
+</div>
+<h1>${data.title}</h1>
+
+<details id="help-with-nationality" class="govuk-details lbh-details" data-module="govuk-details" open>
+  <summary class="govuk-details__summary">
+    <span class="govuk-details__summary-text"> Description </span>
+  </summary>
+  <div class="govuk-details__text">
+  ${data.description}
+  </div>
+</details>
 <details id="help-with-nationality" class="govuk-details lbh-details" data-module="govuk-details" open>
   <summary class="govuk-details__summary">
     <span class="govuk-details__summary-text"> Relationships </span>
@@ -21,8 +40,8 @@ class Entities {
   ${relationships}
   </div>
 </details>
-${data.content ? 
-`
+${data.content ?
+            `
 <details id="help-with-nationality" class="govuk-details lbh-details" data-module="govuk-details" open>
 <summary class="govuk-details__summary">
   <span class="govuk-details__summary-text"> Notes </span>
@@ -31,9 +50,7 @@ ${data.content ?
 ${data.content}
 </div>
 </details>` : ''
-}
+        }
 `;
     }
 }
-
-module.exports = Entities;
