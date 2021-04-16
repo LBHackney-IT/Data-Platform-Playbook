160ba351469467faf56c819c003a7514b50cec7f
mattbee <matt.bee@madetech.com>
2021-04-16 14:15:25 +0100

    Add more structured layout to the playbook layout
    This adds columns and tidies up the layout of the data platform
    documentation.
    
    Co-authored-by: maysakanoni <maysa@madetech.com>
    Co-authored-by: b-dalton <ben.dalton@madetech.com>

--- pages/_includes/playbook_js.11ty.js
+++ pages/_includes/playbook_js.11ty.js
@@ -10,36 +10,30 @@ class Playbook {
         console.log("title", data.title)
         console.log("===============")
         return `
-<div class="govuk-breadcrumbs lbh-breadcrumbs lbh-container">
-    <ol class="govuk-breadcrumbs__list">
-        <li class="govuk-breadcrumbs__list-item"><a class="govuk-breadcrumbs__link" href=${this.url("/")}>Home</a></li>
-        <li class="govuk-breadcrumbs__list-item" aria-current="page">${data.title}</li>
-    </ol>
-</div>
-<h1>${data.title}</h1>
+          <div class="govuk-breadcrumbs lbh-breadcrumbs lbh-container">
+              <ol class="govuk-breadcrumbs__list">
+                  <li class="govuk-breadcrumbs__list-item"><a class="govuk-breadcrumbs__link" href=${this.url("/")}>Home</a></li>
+                  <li class="govuk-breadcrumbs__list-item" aria-current="page">${data.title}</li>
+              </ol>
+          </div>
+          <h1 class="lbh-heading-h1">${data.title}</h1>
 
-<details id="help-with-nationality" class="govuk-details lbh-details" data-module="govuk-details" open>
-  <summary class="govuk-details__summary">
-    <span class="govuk-details__summary-text"> Description </span>
-  </summary>
-  <div class="govuk-details__text">
-  ${data.description}
-  </div>
-</details>
-${data.content ?
-            `
-<details id="help-with-nationality" class="govuk-details lbh-details" data-module="govuk-details" open>
-<summary class="govuk-details__summary">
-  <span class="govuk-details__summary-text"> Notes </span>
-</summary>
-<div class="govuk-details__text">
-${data.content}
-</div>
-</details>` : ''
-        }
-`;
+            <div class="lbh-playbook-layout">
+              <div class="lbh-body lbh-playbook-nav">
+                <p>${data.description}</p>
+                <h4>Navigation</h4>
+                <ol class="lbh-list lbh-list--number">
+                  <li>
+                    <a class="lbh-link" href=${this.url("/playbook/google-sheets-import/")}>Importing Google Sheets</a>
+                  </li>
+                </ol>
+              </div>
+              <div class="lbh-body lbh-playbook-content">
+                ${data.content ? data.content : ''}
+              </div>
+            </div>
+        `
     }
 }
 
 module.exports = Playbook;
-
